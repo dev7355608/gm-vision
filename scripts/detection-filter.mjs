6f113ac1e5ec8d393688e5411acc5f6caebc0c6d
dev7355608 <dev7355608@gmail.com>
2026-07-26 13:22:53 +0200

    Fix deprecation warnings

--- scripts/detection-filter.mjs
+++ scripts/detection-filter.mjs
@@ -1,49 +1,74 @@
 export default class DetectionFilter extends foundry.canvas.rendering.filters.AbstractBaseFilter {
     /** @override */
-    static vertexShader = `\
-        attribute vec2 aVertexPosition;
+    static _createVertexShader(options) {
+        return `
+            attribute vec2 aVertexPosition;
 
-        uniform vec4 inputSize;
-        uniform vec4 outputFrame;
-        uniform mat3 projectionMatrix;
-        uniform vec2 origin;
-        uniform mediump float thickness;
+            uniform vec4 inputSize;
+            uniform vec4 outputFrame;
+            uniform mat3 projectionMatrix;
+            uniform vec2 origin;
+            uniform mediump float thickness;
 
-        varying vec2 vTextureCoord;
-        varying float vOffset;
+            varying vec2 vTextureCoord;
+            varying float vOffset;
 
-        void main() {
-            vTextureCoord = (aVertexPosition * outputFrame.zw) * inputSize.zw;
-            vec2 position = aVertexPosition * max(outputFrame.zw, vec2(0.0)) + outputFrame.xy;
-            vec2 offset = position - origin;
-            vOffset = (offset.x + offset.y) / (1.414213562373095 * 2.0 * thickness);
-            gl_Position = vec4((projectionMatrix * vec3(position, 1.0)).xy, 0.0, 1.0);
-        }
-    `;
+            void main() {
+                vTextureCoord = (aVertexPosition * outputFrame.zw) * inputSize.zw;
+                vec2 position = aVertexPosition * max(outputFrame.zw, vec2(0.0)) + outputFrame.xy;
+                vec2 offset = position - origin;
+                vOffset = (offset.x + offset.y) / (1.414213562373095 * 2.0 * thickness);
+                gl_Position = vec4((projectionMatrix * vec3(position, 1.0)).xy, 0.0, 1.0);
+            }
+        `;
+    }
 
     /** @override */
-    static fragmentShader = `\
-        varying vec2 vTextureCoord;
-        varying float vOffset;
+    static _createFragmentShader(options) {
+        return `
+            varying vec2 vTextureCoord;
+            varying float vOffset;
 
-        uniform sampler2D uSampler;
-        uniform mediump float thickness;
+            uniform sampler2D uSampler;
+            uniform mediump float thickness;
 
-        void main() {
-            float x = abs(vOffset - floor(vOffset + 0.5)) * 2.0;
-            float y0 = clamp((x + 0.5) * thickness + 0.5, 0.0, 1.0);
-            float y1 = clamp((x - 0.5) * thickness + 0.5, 0.0, 1.0);
-            float y = y0 - y1;
-            float alpha = texture2D(uSampler, vTextureCoord).a * 0.25;
-            gl_FragColor = vec4(y, y, y, 1.0) * alpha;
-        }
-    `;
+            void main() {
+                float x = abs(vOffset - floor(vOffset + 0.5)) * 2.0;
+                float y0 = clamp((x + 0.5) * thickness + 0.5, 0.0, 1.0);
+                float y1 = clamp((x - 0.5) * thickness + 0.5, 0.0, 1.0);
+                float y = y0 - y1;
+                float alpha = texture2D(uSampler, vTextureCoord).a * 0.25;
+                gl_FragColor = vec4(y, y, y, 1.0) * alpha;
+            }
+        `;
+    }
 
     /** @override */
     static defaultUniforms = {
         origin: { x: 0.0, y: 0.0 },
         thickness: 1.0,
     };
+
+    static {
+        Hooks.once("init", () => {
+            if (game.release.generation >= 14) {
+                return;
+            }
+
+            Object.defineProperties(DetectionFilter, {
+                vertexShader: {
+                    get() {
+                        return this._createVertexShader();
+                    },
+                },
+                fragmentShader: {
+                    get() {
+                        return this._createFragmentShader();
+                    },
+                },
+            });
+        });
+    }
 
     /** @override */
     apply(filterManager, input, output, clearMode, currentState) {
